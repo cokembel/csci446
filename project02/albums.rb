0269509bdd43b80159a458ad3802bd8b960852e3
Cole Kembel <cokembel@mines.edu>
2013-01-29 05:28:29 -0700

    Adding albumForm.html. Added some methods to albums.rb

--- project02/albums.rb
+++ project02/albums.rb
@@ -1,9 +1,28 @@
+#!/usr/bin/env ruby
 require 'rack'
 
-class HelloWorld
+class Albums
+  
+  @albums = Array.new;
+
   def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
+  	request = Rack::Request.new(env)
+  	case request.path 
+  	when "/display_list" then render_list(request)
+  	end
+
+    #[200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
+  end
+
+  def render_list(request)
+  	response = Rack::Response.new(request.path)
+  	response.finish
+  end
+
+  def readAlbums
+  	albums = File.read('top_100_albums.txt')
+  
   end
 end
 
-Rack::Handler::WEBrick.run HelloWorld.new, :Port => 8080
+Rack::Handler::WEBrick.run Albums.new, :Port => 8080
