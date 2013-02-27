ab6827cb8d21f5b79d57482f61f794df2dbee4e1
Cole Kembel <cokembel@mines.edu>
2013-02-27 15:23:46 -0700

    Modified xxx_create_products.rb in db/migrate to have needed attributes

--- project04/depot/db/migrate/20130226202729_create_products.rb
+++ project04/depot/db/migrate/20130226202729_create_products.rb
@@ -1,8 +1,11 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
-      t.timestamps
+    	t.string :title
+    	t.text :descritption
+    	t.string :image_url
+    	t.decimal :price, :precision => 8, :scale => 2
+     	t.timestamps
     end
   end
 end
