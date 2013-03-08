6bfb0a6b27e6df7d53a51c5c4b895ff54d3e98ad
Cole Kembel <cokembel@mines.edu>
2013-03-08 11:29:14 -0700

    Added three fixtures to test/fixtures/products.yml

--- project04/depot/test/unit/product_test.rb
+++ project04/depot/test/unit/product_test.rb
@@ -23,6 +23,20 @@ class ProductTest < ActiveSupport::TestCase
   	assert product.valid?
   end
 
+  test "image url" do
+  	ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.jpg
+  			http://a.b.c/x/y/z/fred.gif }
+  	bad = %w{ fred.doc fred.gif/more frid.gif.more }
+
+  	ok.each do |name|
+  		assert new_product(name).valid?, "#{name} shouldn't be valid"
+  	end
+
+  	bad.each do |name|
+  		assert new_product(name).invalud?, "#{name} shouldn't be valid"
+  	end
+  end
+
   def new_product(image_url)
   	Product.new(:title => "My Book Title",
   				:descritption => "yyy",
