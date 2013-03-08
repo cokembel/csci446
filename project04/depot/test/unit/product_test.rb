f9d0e8459b2836e7a2ead915da9d053139f309f6
Cole Kembel <cokembel@mines.edu>
2013-03-08 11:08:13 -0700

    Added unit tests for having non empty attributes and for having a positive price value in test/unit/product_test.erb

--- project04/depot/test/unit/product_test.rb
+++ project04/depot/test/unit/product_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+	  product = Product.new
+	  assert product.invalid?
+	  assert product.errors[:title].any?
+	  assert product.errors[:descritption].any?
+	  assert product.errors[:price].any?
+	  assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+  	product = Product.new(:title => "My Book Title",
+  						  :descritption => "yyy",
+  						  :image_url => "zzz.jpg")
+  	product.price = -1
+  	assert product.invald?
+  	assert_equal "must be greater than or equal to 0.01", 
+  		product.error[:price].join('; ')
+
+  	product.price = 1
+  	assert product.valid?
+  end
+
+  def new_product(image_url)
+  	Product.new(:title => "My Book Title",
+  				:descritption => "yyy",
+  				:price => 1,
+  				:image_url => image_url)
+  end
 end
